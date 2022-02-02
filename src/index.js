32991b1f267ffc11076773f5de29f6a7e29ec6d6
kkoouuya <koya.iwabuchi@rakus-partners.co.jp>
2022-02-02 11:17:38 +0900

    install eval-source-map

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 
-console.log("iam printing log");
-
 ReactDOM.render(<div>Hello, React</div>, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -66,4 +66,5 @@ module.exports = {
       new OptimizeCSSAssetsPlugin({}),
     ],
   },
+  devtool: 'eval-source-map'
 };
